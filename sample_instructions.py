e60f096c871e558abceeb4c23e7d6ff2dcb2f8a5
Joshua Mitchener <jmitchen@uci.edu>
2021-02-23 16:32:08 -0800

    Add Instructions File

---
+++ sample_instructions.py
@@ -0,0 +1,49 @@
+# 
+# THIS IS A SAMPLE OF DASHMIPS INSTRUCTION FUNCTIONS
+# 
+
+"""Three Register instructions."""
+from typing import Tuple
+from re import search
+
+from . import mips_instruction
+from ..models import MipsProgram
+
+PATTERN = r"{instr_gap}({register}){args_gap}({register}){args_gap}({register})"
+
+
+def parse(args: Tuple[str, str, str, str, str]) -> Tuple[str, str, str]:
+    """Parser for rd rs rt format instructions."""
+    return (args[2], args[3], args[4])
+
+
+@mips_instruction(PATTERN, parse)
+def add(program: MipsProgram, rd: str, rs: str, rt: str):
+    """Add Reg[rd] = Reg[rs] + Reg[rt]."""
+    program.registers[rd] = program.registers[rs] + program.registers[rt]
+
+
+@mips_instruction(PATTERN, parse)
+def addu(program: MipsProgram, rd: str, rs: str, rt: str):
+    """Add unsigned Reg[rd] = Reg[rs] + Reg[rt]."""
+    program.registers[rd] = program.registers[rs] + program.registers[rt]
+
+
+@mips_instruction(PATTERN, parse)
+def _and(program: MipsProgram, rd: str, rs: str, rt: str):
+    """Bitwise And Reg[rd] = Reg[rs] & Reg[rt]."""
+    program.registers[rd] = program.registers[rs] & program.registers[rt]
+
+
+@mips_instruction(PATTERN, parse)
+def movn(program: MipsProgram, rd: str, rs: str, rt: str):
+    """Move conditional Reg[rd] = Reg[rs] if Reg[rt] != 0."""
+    if program.registers[rt] != 0:
+        program.registers[rd] = program.registers[rs]
+
+
+@mips_instruction(PATTERN, parse)
+def movz(program: MipsProgram, rd: str, rs: str, rt: str):
+    """Move conditional Reg[rd] = Reg[rs] if Reg[rt] == 0."""
+    if program.registers[rt] == 0:
+        program.registers[rd] = program.registers[rs]
